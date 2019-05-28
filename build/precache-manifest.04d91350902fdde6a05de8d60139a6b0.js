08f1b8232feaaab7a0ff3474516d9c4e6fc87beb
KevinSarav <38804180+KevinSarav@users.noreply.github.com>
2019-05-28 11:35:38 -0500

    All Files

---
+++ build/precache-manifest.04d91350902fdde6a05de8d60139a6b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdd27a04bbda712893d60ae346a33a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a0dd47fbd79358f32d1b",
+    "url": "/static/css/main.66c3e744.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "a0dd47fbd79358f32d1b",
+    "url": "/static/js/main.49c29200.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
